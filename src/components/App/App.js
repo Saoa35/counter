c9c60fa9a8615264ef1d6ffc3e7ca2f2cd70224b
Saoa35 <saoamah@gmail.com>
2022-10-02 14:42:51 +0200

    added UseState

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [count, setCount] = useState(null);
+
+
+
   return (
     <div className="App">
-      
+
+      <h2>Counter</h2>
+      <h1>{count}</h1>
+      <button onClick={handleClickPlus}>- Minus</button>
+      <button onClick={handleClickMinus}>Plus +</button>
+
     </div>
   );
 }
