845d0706438dd3b8bf7695d87cd8d8745fda6b0d
Saoa35 <saoamah@gmail.com>
2022-10-02 15:06:10 +0200

    installed sass library

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import { useState } from 'react';
-import './App.css';
+import '../../index.scss'
 
 function App() {
 
   const [count, setCount] = useState(null);
 
+  const handleClickPlus = () => {
+    setCount(count + 1);
+  }
 
+  const handleClickMinus = () => {
+    setCount(count - 1);
+  }
 
   return (
     <div className="App">
 
       <h2>Counter</h2>
       <h1>{count}</h1>
-      <button onClick={handleClickPlus}>- Minus</button>
-      <button onClick={handleClickMinus}>Plus +</button>
+      <button onClick={handleClickPlus}>Plus +</button>
+      <button onClick={handleClickMinus}>- Minus</button>
 
     </div>
   );
