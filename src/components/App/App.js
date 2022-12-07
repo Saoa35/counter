1e0db6802b52d7733b16c0e0aea651ea74756cf2
Saoa35 <saoamah@gmail.com>
2022-12-07 21:04:32 +0200

    changed setCount state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,31 +1,32 @@
-import React from 'react';
-import { useState } from 'react';
-import '../../index.css'
+import React from "react";
+import { useState } from "react";
+import "../../index.css";
 
 function App() {
-
   const [count, setCount] = useState(0);
 
   const handleClickPlus = () => {
-    setCount(count + 1);
-  }
+    setCount((prev) => prev + 1);
+  };
 
   const handleClickMinus = () => {
-    setCount(count - 1);
-  }
+    setCount((prev) => prev - 1);
+  };
 
   return (
     <div className="App">
-
-      <div className='main'>
+      <div className="main">
         <div>
-        <h2>Counter</h2>
-        <h1>{count}</h1>
-        <button onClick={handleClickPlus} className='plus'>Plus +</button>
-        <button onClick={handleClickMinus} className='minus'>- Minus</button>
+          <h2>Counter</h2>
+          <h1>{count}</h1>
+          <button onClick={handleClickPlus} className="plus">
+            Plus +
+          </button>
+          <button onClick={handleClickMinus} className="minus">
+            - Minus
+          </button>
         </div>
       </div>
-
     </div>
   );
 }
